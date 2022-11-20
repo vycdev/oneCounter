3d684aad8926f9627009e613ede7ba2dfc4edf60
Victor-Iulian Sandu <vyctor661@gmail.com>
2022-11-20 07:22:19 +0200

    feat: charts

--- src/screens/stats/StatsScreen.tsx
+++ src/screens/stats/StatsScreen.tsx
@@ -1,22 +1,74 @@
-import { StyleSheet } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
+import { BarChart } from "react-native-chart-kit";
 
 import EditScreenInfo from "../../components/EditScreenInfo";
 import { Text, View } from "../../components/Themed";
 import { RootTabScreenProps } from "../../types";
 
+const data = {
+    labels: ["January", "February", "March", "April", "May", "June"],
+    datasets: [
+        {
+            data: [20, 45, 28, 80, 99, 43],
+        },
+    ],
+};
+
 export default function StatsScreen() {
-    return <View style={styles.container}></View>;
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>Your Spendings</Text>
+            <BarChart
+                // style={graphStyle}
+                data={data}
+                width={Dimensions.get("screen").width - 50}
+                height={300}
+                yAxisLabel="$"
+                chartConfig={{
+                    backgroundColor: "#000000",
+                    backgroundGradientFrom: "#1E2923",
+                    backgroundGradientTo: "#08130D",
+                    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+                    style: {
+                        borderRadius: 16,
+                    },
+                }}
+                verticalLabelRotation={20}
+                yAxisSuffix={""}
+            />
+            <Text style={styles.title}>Your Spendings</Text>
+            <BarChart
+                // style={graphStyle}
+                data={data}
+                width={Dimensions.get("screen").width - 50}
+                height={300}
+                yAxisLabel="$"
+                chartConfig={{
+                    backgroundColor: "#000000",
+                    backgroundGradientFrom: "#1E2923",
+                    backgroundGradientTo: "#08130D",
+                    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+                    style: {
+                        borderRadius: 16,
+                    },
+                }}
+                verticalLabelRotation={20}
+                yAxisSuffix={""}
+            />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
+        // flex: 1,
         alignItems: "center",
         justifyContent: "center",
     },
     title: {
         fontSize: 20,
         fontWeight: "bold",
+        margin: 20,
     },
     separator: {
         marginVertical: 30,
